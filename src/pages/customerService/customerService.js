eef0ddcf0d17391d2de299ae70ea511c2a233695
jzm <somedayjzm@foxmail.com>
2022-08-30 18:33:32 +0800

    udpate: 安装a-hooks，初体验

--- src/pages/customerService/customerService.js
+++ src/pages/customerService/customerService.js
@@ -1,7 +1,8 @@
 import styles from './customerService.less'; // 引入页面样式文件
 
 import React, { useEffect, useState } from 'react';
-import { Input, Select, Button, Table, Pagination } from 'antd'; // 引入antd组件
+import { Input, Select, Button, Table, Pagination, message } from 'antd'; // 引入antd组件
+import { useMount, useUnmount } from 'ahooks'; // 引入ahooks钩子函数
 
 import { getAiServiceList } from '@/services/ant-design-pro/customer'; // 引入接口
 
@@ -151,17 +152,32 @@ export default () => {
    * useEffect 相当于vue的created 和 mounted 钩子 一般用于请求数据 以及初始化数据
    * 以及监听事件 等等 一般不会在这里面写业务逻辑 业务逻辑写在其他函数里面 然后在这里面调用
    */
-  useEffect(() => {
-    getTableData();
-    return () => {
-      /**
-       * 就是这个 return 相当于vue的beforeDestroy unmount 钩子相关
-       * 一般用于清除监听事件 等等 一般不会在这里面写业务逻辑 业务逻辑写在其他函数里面 然后在这里面调用
-       */
-      console.log('人工客服页面卸载了');
-    };
-  }, []);
+  // useEffect(() => {
+  //   getTableData();
+  //   return () => {
+  //     /**
+  //      * 就是这个 return 相当于vue的beforeDestroy unmount 钩子相关
+  //      * 一般用于清除监听事件 等等 一般不会在这里面写业务逻辑 业务逻辑写在其他函数里面 然后在这里面调用
+  //      */
+  //     console.log('人工客服页面卸载了');
+  //   };
+  // }, []);
 
+  /**
+   *  这是 另外一种 生命周期的写法。感觉这种更好， 挂载 和 卸载 分开写 更好
+   *  a-hooks 相关  一般用于请求数据 以及初始化数据 ，
+   *  这是mounted 钩子
+   */
+  useMount(() => {
+    getTableData();
+    message.info('我是mounted');
+  });
+  /**
+   * 这里是 unmount 钩子
+   */
+  useUnmount(() => {
+    message.info('我是unmount--卸载页面');
+  });
   return (
     <>
       <h1>人工客服</h1>
